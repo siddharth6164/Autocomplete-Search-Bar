d9a9c30466a1aebc9b6d075230f424096c10135f
Siddharth Singh <siddhu33singh@gmail.com>
2025-03-01 00:17:56 +0530

    Autocomplete Search Bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,29 @@
-import { useState } from 'react'
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
-  return (
-    <div className='App'>
+  const [input, setInput] = useState("");
+  const [results, setResults] = useState([]);
 
+  const fetchData = async () => {
+    const data = await fetch("https://dummyjson.com/recipes/search?q=");
+    const json = await data.json();
+    setResults(json.recipes)
+  };
+  useEffect(()=>{fetchData()},[input]);
+  return (
+    <div className="App">
+      <h1>Autocomplete Search Bar</h1>
+      <div>
+        <input
+          type="text"
+          className="search-input"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        />
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
