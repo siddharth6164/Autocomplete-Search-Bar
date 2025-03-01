117107a8748392a855c75a9b4d825ba4839ed565
Siddharth Singh <siddhu33singh@gmail.com>
2025-03-01 19:57:46 +0530

    Autocomplete Search Bar

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,21 @@ import "./App.css";
 function App() {
   const [input, setInput] = useState("");
   const [results, setResults] = useState([]);
-
+  const [showResults,setShowResults] = useState(false);
   const fetchData = async () => {
-    const data = await fetch("https://dummyjson.com/recipes/search?q=");
+    console.log("API",input);
+    const data = await fetch("https://dummyjson.com/recipes/search?q="+input);
     const json = await data.json();
-    setResults(json.recipes)
+    setResults(json.recipes);
   };
-  useEffect(()=>{fetchData()},[input]);
+
+  useEffect(() => {
+    const timer = setTimeout(fetchData,400);
+    return ()=>{
+      clearTimeout(timer);
+    }
+  }, [input]);
+
   return (
     <div className="App">
       <h1>Autocomplete Search Bar</h1>
@@ -20,7 +28,16 @@ function App() {
           className="search-input"
           value={input}
           onChange={(e) => setInput(e.target.value)}
+          onFocus={()=>{setShowResults(true)}}
+          onBlur={()=>{setShowResults(false)}}
         />
+        {showResults &&(
+          <div className="results-container">
+          {results.map((r) => (
+            <span className="result" key={r.id}>{r.name}</span>
+          ))}
+        </div>
+        )}
       </div>
     </div>
   );
